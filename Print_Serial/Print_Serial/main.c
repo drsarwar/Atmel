5c05c1e40b7a89744aa33e2e9e62dadebad4613f
drsarwar <63023354+drsarwar@users.noreply.github.com>
2020-07-29 18:58:49 -0700

    moved the print line only inside while loop and kept the initialization of serial port at the beginning

--- Print_Serial/Print_Serial/main.c
+++ Print_Serial/Print_Serial/main.c
@@ -8,16 +8,22 @@ void UART_EDBG_init(void)
 	usart_sync_get_io_descriptor(&USART_0, &uart_edbg_io);
 	usart_sync_enable(&USART_0);
 
-	io_write(uart_edbg_io, (uint8_t *)"Hello World!\n", 14);
+	//io_write(uart_edbg_io, (uint8_t *)"Hello World!\n", 14);
+	//io_write(uart_edbg_io, (uint8_t *)"Hello WORLS!\n", 14);
 }
 
 int main(void)
 {
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
-
+	
+	struct io_descriptor *uart_edbg_io;
+	usart_sync_get_io_descriptor(&USART_0, &uart_edbg_io);
+	usart_sync_enable(&USART_0);
+	
 	/* Replace with your application code */
 	while (1) {
-		UART_EDBG_init();
+		io_write(uart_edbg_io, (uint8_t *)"Hello WORLS!\n", 14);
+		//UART_EDBG_init();
 	}
 }
